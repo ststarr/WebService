7b291d2afa030719ff2b7fe3b749454d458dc440
Al Hammons <susan.tamilyn@gmail.com>
2016-09-13 17:18:13 -0500

    EOD 9/13/2016 web service project..svc called, data returned but not formatted yet

--- Web Service Example/ViewController.swift
+++ Web Service Example/ViewController.swift
@@ -4,15 +4,30 @@
 //
 //  Created by Al Hammons on 9/13/16.
 //  Copyright © 2016 Susan Hammons. All rights reserved.
+//  9/13/2016 app id for ststarr user = bbbd58c85a79eb93bc78596c07f681cf
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var forecastLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let manager = AFHTTPSessionManager()
+        manager.GET("http://api.openweathermap.org/data/2.5/forecast/daily?q=London&mode=json&units=metric&cnt=1&appid=bbbd58c85a79eb93bc78596c07f681cf",
+                    parameters: nil,
+                    progress: nil,
+                    success: { (operation: NSURLSessionDataTask,responseObject: AnyObject?) in
+                        if let responseObject = responseObject {
+                            print("Response: " + responseObject.description)
+                        }
+            },
+                    failure: { (operation: NSURLSessionDataTask?,error: NSError) in
+                        print("Error: " + error.localizedDescription)
+        })
     }
 
     override func didReceiveMemoryWarning() {
---
+++ bridge.h
@@ -0,0 +1,15 @@
+//
+//  bridge.h
+//  Web Service Example
+//
+//  Created by Al Hammons on 9/13/16.
+//  Copyright © 2016 Susan Hammons. All rights reserved.
+//
+
+#import "AFNetworking/AFNetworking.h"
+
+#ifndef bridge_h
+#define bridge_h
+
+
+#endif /* bridge_h */
